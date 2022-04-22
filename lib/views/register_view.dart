328abb653e1112cfd7666979b248c0d87123939a
unknown <jjcamacho0410@gmail.com>
2022-04-22 16:06:45 -0600

    fifth commit

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -29,7 +30,7 @@ class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Register')),
+      appBar: AppBar(title: const Text('Register')),
       body: Column(
         children: [
           TextField(
@@ -56,14 +57,14 @@ class _RegisterViewState extends State<RegisterView> {
                   final userCredential = await FirebaseAuth.instance
                       .createUserWithEmailAndPassword(
                           email: email, password: password);
-                  print(userCredential);
+                  devtools.log(userCredential.toString());
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'weak-password') {
-                    print('weak password');
+                    devtools.log('weak password');
                   } else if (e.code == 'email-already-in-use') {
-                    print('Email is already in use');
+                    devtools.log('Email is already in use');
                   } else if (e.code == 'invalid-email') {
-                    print('invalid email entered');
+                    devtools.log('invalid email entered');
                   }
                 }
               },
@@ -73,7 +74,7 @@ class _RegisterViewState extends State<RegisterView> {
                 Navigator.of(context)
                     .pushNamedAndRemoveUntil('/login/', (route) => false);
               },
-              child: Text('Already registered? Login here!'))
+              child: const Text('Already registered? Login here!'))
         ],
       ),
     );
